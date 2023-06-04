d7ad6f4c42e664dbc984008744cfabbfad193f3e
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-06-04 13:14:17 -0700

    created instance of ApolloServer and passed in typeDefs and resolvers, set up Apollo server middleware, and connected to the MongoDB database

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,8 +22,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`GraphQL API ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
