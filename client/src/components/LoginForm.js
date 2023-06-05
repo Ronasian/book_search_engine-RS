6b19f00533ca28e80c5d774342503aa6b06d2f0f
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-06-04 17:51:02 -0700

    refactored loginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,16 @@
-// see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
+  const [validated, setValidated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  // Apollo useMutation Hook to execute the LOGIN_USER mutation
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,13 +28,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
+      const { token, user } = data.loginUser;
       console.log(user);
       Auth.login(token);
     } catch (err) {
@@ -40,8 +40,8 @@ const LoginForm = () => {
       setShowAlert(true);
     }
 
+    setValidated(true);
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
